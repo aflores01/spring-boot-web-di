a986db825ebe558a5128aa30d20208a49184d4e1
karunsane <karunsane@gmail.com>
2020-06-27 00:50:03 -0500

    Conflicto resuelto

--- src/main/java/com/learning/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/learning/springboot/di/app/models/domain/Factura.java
@@ -1,7 +1,6 @@
 package com.learning.springboot.di.app.models.domain;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
